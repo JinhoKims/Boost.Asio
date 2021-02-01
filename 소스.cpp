900064fc04ddd3e622a0060d142d21873fd114b7
Jinho Kim <55690757+JinhoKims@users.noreply.github.com>
2021-02-01 20:54:10 +0900

    update

--- 소스.cpp
+++ 소스.cpp
@@ -7,20 +7,20 @@ const unsigned short PORT_NUMBER = 31400;
 
 int main()
 {
-	// 동기형 방식을 사용하는 프로그램
+	// 동기형 방식을 사용하는 프로그램(서버)
 	boost::asio::io_service io_service; // I/O 이벤트를 OS에서 디스패치하는 객체
 	boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::tcp::v4(), PORT_NUMBER); // 네트워크 주소(포트) 설정
-	boost::asio::ip::tcp::acceptor acceptor(io_service, endpoint); // 클라이언트의 접속을 담당
+	boost::asio::ip::tcp::acceptor acceptor(io_service, endpoint); // 클라이언트의 접속을 담당하는 객체
 
 	boost::asio::ip::tcp::socket socket(io_service); // 접속한 클라이언트에 할당할 소켓(제어용)
 	acceptor.accept(socket); // 클라이언트의 접속을 담당하는 acceptor에 socket을 넘겨줌, 동기형이므로 접속이 완료될 때까지 대기함
-	cout << "클라이언트 접속" << endl;
-	
+	cout << "클라이언트가 접속함" << endl;
+
 	for (;;)
 	{
-		// 데이터 수신
+		// 데이터 수신 read_some
 		std::array<char, 128> buf; // 수신용 메시지 버퍼
-		buf.assign(0);
+		buf.assign(0); // 버퍼 초기화
 		boost::system::error_code error; // 에러 클래스
 		size_t len = socket.read_some(boost::asio::buffer(buf), error); // 클라이언트가 보낸 데이터를 받음, 동기형이므로 데이터를 다 받을때까지 기다림
 		
@@ -39,7 +39,7 @@ int main()
 
 		cout << "클라이언트에서 받은 메시지 : " << &buf[0] << endl; // 받은 메시지 출력
 
-		// 데이터 송신
+		// 데이터 송신 write_some
 		char szMessage[128] = { 0, }; // 발신용 데이터
 		sprintf_s(szMessage, 128 - 1, "Re:%s", &buf[0]); // szMessage에 수신된 buf 값을 넣어줌
 		int nMsgLen = strnlen_s(szMessage, 128 - 1); // 메시지 길이 계산
